31e013589464c9d0c8f0d23f15c6d44cf5a58ae0
ahmad.al-naib <alnaib888@gmail.com>
2024-08-06 16:15:35 +0200

    make the login and register the same page

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { ref } from 'vue';
 import SignupForm from '@/components/SignupForm.vue';
 import LoginForm from '@/components/LoginForm.vue';
 
+const showLogin = ref(true);
 </script>
 
 <template>
   <main>
-    <SignupForm />
-    <LoginForm />
+    <div v-if="showLogin">
+      <LoginForm />
+      <button @click="showLogin = false" class="block mx-auto mt-4 text-blue-500">Don't have an account? Sign up</button>
+    </div>
+    <div v-else>
+      <SignupForm />
+      <button @click="showLogin = true" class="block mx-auto mt-4 text-blue-500">Already have an account? Login</button>
+    </div>
   </main>
 </template>
