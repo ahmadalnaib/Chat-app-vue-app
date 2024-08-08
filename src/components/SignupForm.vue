cdab4f77e42faccf6290f381cb0afe3cd26cfac2
ahmad.al-naib <alnaib888@gmail.com>
2024-08-08 16:21:34 +0200

    show the error MSG

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,23 +3,19 @@ import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import useSignup from '../composables/useSignup';
 
-
-
 const name = ref('');
 const email = ref('');
 const password = ref('');
 
 const { signup, error } = useSignup();
 
-const router=useRouter();
+const router = useRouter();
 
 const handleSubmit = async () => {
-  await signup( email.value, password.value);
-  if(!error.value)
-{
-router.push('/')
-
-}
+  await signup(email.value, password.value);
+  if (!error.value) {
+    router.push('/');
+  }
 };
 </script>
 
@@ -53,6 +49,9 @@ router.push('/')
           v-model="password"
           class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
+      </div>
+      <div v-if="error" class="text-red-500 m-5">
+        {{ error }}
       </div>
       <button
         class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
